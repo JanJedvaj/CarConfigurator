839586fe939b5650609d8a303bc6f7618aa4c447
JanJedvaj <jan.jedvaj2@gmail.com>
2026-01-14 15:09:29 +0100

    Add register button and change password option

---
+++ CarConfigurator-WebApp/ViewModels/ChangePasswordVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarConfigurator_WebApp.ViewModels
+{
+    public class ChangePasswordVM
+    {
+        [Required(ErrorMessage = "Stara lozinka je obavezna.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Old password")]
+        public string OldPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Nova lozinka je obavezna.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Nova lozinka mora imati barem 8 znakova.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Potvrda nove lozinke je obavezna.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare(nameof(NewPassword), ErrorMessage = "Nove lozinke se ne podudaraju.")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
---
+++ CarConfigurator-WebApp/ViewModels/RegisterVM.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarConfigurator_WebApp.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Username je obavezan.")]
+        [StringLength(100)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email je obavezan.")]
+        [EmailAddress(ErrorMessage = "Email nije ispravan.")]
+        [StringLength(255)]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Lozinka je obavezna.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Lozinka mora imati barem 6 znakova.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Potvrda lozinke je obavezna.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare(nameof(Password), ErrorMessage = "Lozinke se ne podudaraju.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
